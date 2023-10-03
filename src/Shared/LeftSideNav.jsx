52154585ac9127e7203dabcabac01d117f1754fa
Masum-Reza <careerhunter4280@gmail.com>
2023-10-03 13:33:24 -0700

    rightside nav completed

--- src/Shared/LeftSideNav.jsx
+++ src/Shared/LeftSideNav.jsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react"
+import { NavLink } from "react-router-dom"
+import './leftnav.css'
+
 const LeftSideNav = () => {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    fetch('/categories.json')
+      .then(res => res.json())
+      .then(data => setCategories(data))
+  }, [])
+  console.log(categories)
+
   return (
     <div>
-      left side nav
+      <h1 className="font-bold">All Caterogy</h1>
+      <div className="space-y-4 mt-5">
+        {
+          categories.map(category => {
+            let { name, id } = category;
+            return <div className="text-justify pl-12 text-lg" key={id}>
+              <NavLink to={`category/${id}`}>{name}</NavLink>
+            </div>
+          })
+        }
+      </div>
     </div>
   )
 }
